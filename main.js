8175e864b08652692a386ce73c0118d8ff5d6d38
alexdewaal66 <alexdewaal@gmail.com>
2021-04-29 13:50:32 +0200

    opdracht 5c gedaan

--- main.js
+++ main.js
@@ -1,181 +1,181 @@
 // VOORRAAD ARRAY MET TV'S
 const inventory = [
-  {
-    type: '43PUS6504/12',
-    name: '4K TV',
-    brand: 'Philips',
-    price: 379,
-    availableSizes: [43, 50, 58, 65],
-    refreshRate: 50,
-    screenType: 'LED',
-    screenQuality: 'Ultra HD/4K',
-    smartTv: true,
-    options: {
-      wifi: true,
-      speech: false,
-      hdr: true,
-      bluetooth: false,
-      ambiLight: false,
+    {
+        type: '43PUS6504/12',
+        name: '4K TV',
+        brand: 'Philips',
+        price: 379,
+        availableSizes: [43, 50, 58, 65],
+        refreshRate: 50,
+        screenType: 'LED',
+        screenQuality: 'Ultra HD/4K',
+        smartTv: true,
+        options: {
+            wifi: true,
+            speech: false,
+            hdr: true,
+            bluetooth: false,
+            ambiLight: false,
+        },
+        originalStock: 23,
+        sold: 2,
     },
-    originalStock: 23,
-    sold: 2,
-  },
-  {
-    type: 'NH3216SMART',
-    name: 'HD smart TV',
-    brand: 'Nikkei',
-    price: 159,
-    availableSizes: [32],
-    refreshRate: 100,
-    screenType: 'LED',
-    screenQuality: 'HD ready',
-    smartTv: true,
-    options: {
-      wifi: true,
-      speech: false,
-      hdr: false,
-      bluetooth: false,
-      ambiLight: false,
+    {
+        type: 'NH3216SMART',
+        name: 'HD smart TV',
+        brand: 'Nikkei',
+        price: 159,
+        availableSizes: [32],
+        refreshRate: 100,
+        screenType: 'LED',
+        screenQuality: 'HD ready',
+        smartTv: true,
+        options: {
+            wifi: true,
+            speech: false,
+            hdr: false,
+            bluetooth: false,
+            ambiLight: false,
+        },
+        originalStock: 4,
+        sold: 4,
     },
-    originalStock: 4,
-    sold: 4,
-  },
-  {
-    type: 'QE55Q60T',
-    name: '4K QLED TV',
-    brand: 'Samsung',
-    price: 709,
-    availableSizes: [43, 50, 55, 58, 65],
-    refreshRate: 60,
-    screenType: 'QLED',
-    screenQuality: 'Ultra HD/4K',
-    smartTv: true,
-    options: {
-      wifi: true,
-      speech: true,
-      hdr: true,
-      bluetooth: true,
-      ambiLight: false,
+    {
+        type: 'QE55Q60T',
+        name: '4K QLED TV',
+        brand: 'Samsung',
+        price: 709,
+        availableSizes: [43, 50, 55, 58, 65],
+        refreshRate: 60,
+        screenType: 'QLED',
+        screenQuality: 'Ultra HD/4K',
+        smartTv: true,
+        options: {
+            wifi: true,
+            speech: true,
+            hdr: true,
+            bluetooth: true,
+            ambiLight: false,
+        },
+        originalStock: 7,
+        sold: 0,
     },
-    originalStock: 7,
-    sold: 0,
-  },
-  {
-    type: '43HAK6152',
-    name: 'Ultra HD SMART TV',
-    brand: 'Hitachi',
-    price: 349,
-    availableSizes: [43, 50, 55, 58],
-    refreshRate: 60,
-    screenType: 'LCD',
-    screenQuality: 'Ultra HD/4K',
-    smartTv: true,
-    options: {
-      wifi: true,
-      speech: true,
-      hdr: true,
-      bluetooth: true,
-      ambiLight: false,
+    {
+        type: '43HAK6152',
+        name: 'Ultra HD SMART TV',
+        brand: 'Hitachi',
+        price: 349,
+        availableSizes: [43, 50, 55, 58],
+        refreshRate: 60,
+        screenType: 'LCD',
+        screenQuality: 'Ultra HD/4K',
+        smartTv: true,
+        options: {
+            wifi: true,
+            speech: true,
+            hdr: true,
+            bluetooth: true,
+            ambiLight: false,
+        },
+        originalStock: 5,
+        sold: 5,
     },
-    originalStock: 5,
-    sold: 5,
-  },
-  {
-    type: '50PUS7304/12',
-    name: 'The One 4K TV',
-    brand: 'Philips',
-    price: 479,
-    availableSizes: [43, 50, 55, 58, 65, 70],
-    refreshRate: 50,
-    screenType: 'LED',
-    screenQuality: 'Ultra HD/4K',
-    smartTv: true,
-    options: {
-      wifi: true,
-      speech: true,
-      hdr: true,
-      bluetooth: true,
-      ambiLight: true,
+    {
+        type: '50PUS7304/12',
+        name: 'The One 4K TV',
+        brand: 'Philips',
+        price: 479,
+        availableSizes: [43, 50, 55, 58, 65, 70],
+        refreshRate: 50,
+        screenType: 'LED',
+        screenQuality: 'Ultra HD/4K',
+        smartTv: true,
+        options: {
+            wifi: true,
+            speech: true,
+            hdr: true,
+            bluetooth: true,
+            ambiLight: true,
+        },
+        originalStock: 8,
+        sold: 3,
     },
-    originalStock: 8,
-    sold: 3,
-  },
-  {
-    type: '55PUS7805',
-    name: '4K LED TV',
-    brand: 'Philips',
-    price: 689,
-    availableSizes: [55],
-    refreshRate: 100,
-    screenType: 'LED',
-    screenQuality: 'Ultra HD/4K',
-    smartTv: true,
-    options: {
-      wifi: true,
-      speech: false,
-      hdr: true,
-      bluetooth: false,
-      ambiLight: true,
+    {
+        type: '55PUS7805',
+        name: '4K LED TV',
+        brand: 'Philips',
+        price: 689,
+        availableSizes: [55],
+        refreshRate: 100,
+        screenType: 'LED',
+        screenQuality: 'Ultra HD/4K',
+        smartTv: true,
+        options: {
+            wifi: true,
+            speech: false,
+            hdr: true,
+            bluetooth: false,
+            ambiLight: true,
+        },
+        originalStock: 6,
+        sold: 3,
     },
-    originalStock: 6,
-    sold: 3,
-  },
-  {
-    type: 'B2450HD',
-    name: 'LED TV',
-    brand: 'Brandt',
-    price: 109,
-    availableSizes: [24],
-    refreshRate: 60,
-    screenType: 'LED',
-    screenQuality: 'Full HD',
-    smartTv: false,
-    options: {
-      wifi: false,
-      speech: false,
-      hdr: false,
-      bluetooth: false,
-      ambiLight: false,
+    {
+        type: 'B2450HD',
+        name: 'LED TV',
+        brand: 'Brandt',
+        price: 109,
+        availableSizes: [24],
+        refreshRate: 60,
+        screenType: 'LED',
+        screenQuality: 'Full HD',
+        smartTv: false,
+        options: {
+            wifi: false,
+            speech: false,
+            hdr: false,
+            bluetooth: false,
+            ambiLight: false,
+        },
+        originalStock: 10,
+        sold: 8,
     },
-    originalStock: 10,
-    sold: 8,
-  },
-  {
-    type: '32WL1A63DG',
-    name: 'HD TV',
-    brand: 'Toshiba',
-    price: 161,
-    availableSizes: [32],
-    refreshRate: 50,
-    screenType: 'LED',
-    screenQuality: 'Full HD',
-    smartTv: false,
-    options: {
-      wifi: false,
-      speech: false,
-      hdr: true,
-      bluetooth: false,
-      ambiLight: false,
+    {
+        type: '32WL1A63DG',
+        name: 'HD TV',
+        brand: 'Toshiba',
+        price: 161,
+        availableSizes: [32],
+        refreshRate: 50,
+        screenType: 'LED',
+        screenQuality: 'Full HD',
+        smartTv: false,
+        options: {
+            wifi: false,
+            speech: false,
+            hdr: true,
+            bluetooth: false,
+            ambiLight: false,
+        },
+        originalStock: 10,
+        sold: 8,
     },
-    originalStock: 10,
-    sold: 8,
-  },
 ];
 
 function setLoadTime() {
-  const timeEl = document.getElementById("load-time");
-  timeEl.innerText = new Date().toLocaleTimeString();
+    const timeEl = document.getElementById("load-time");
+    timeEl.innerText = new Date().toLocaleTimeString();
 }
 
 setLoadTime();
 
 // opdracht 1a
 function countItemsToSell() {
-  let total = 0;
-  for (const item of inventory) {
-    total += item.originalStock - item.sold;
-  }
-  return total;
+    let total = 0;
+    for (const item of inventory) {
+        total += item.originalStock - item.sold;
+    }
+    return total;
 }
 
 // opdracht 1b
@@ -196,15 +196,15 @@ console.log(`ambiLightModels:`, ambiLightModels);
 
 // opdracht 2d
 function sortByPrice() {
-  inventory.sort((a, b) => a.price - b.price );
+    inventory.sort((a, b) => a.price - b.price);
 }
 
 sortByPrice();
 console.log(`sorted inventory:`, inventory);
 
 // opdracht 3a
-const reducer3a = (amount, item) => amount + item.price * (item.originalStock - item.sold) ;
-const proceedsTarget  = inventory.reduce( reducer3a, 0 );
+const reducer3a = (amount, item) => amount + item.price * (item.originalStock - item.sold);
+const proceedsTarget = inventory.reduce(reducer3a, 0);
 console.log(`proceedsTarget:`, proceedsTarget);
 
 const proceedsTargetEl = document.createElement('p');
@@ -214,7 +214,7 @@ document.body.appendChild(proceedsTargetEl);
 
 // opdracht 3b
 const reducer3b = (amount, item) => amount + item.price * item.sold;
-const proceedsCurrent = inventory.reduce( reducer3b, 0);
+const proceedsCurrent = inventory.reduce(reducer3b, 0);
 console.log(`proceedsCurrent:`, proceedsCurrent);
 
 const proceedsCurrentEl = document.createElement('p');
@@ -233,7 +233,7 @@ document.body.appendChild(tvType2);
 
 // opdracht 5a
 function summarizeTv(tv) {
-  return tv.brand + ' ' + tv.type + ' - ' + tv.name;
+    return tv.brand + ' ' + tv.type + ' - ' + tv.name;
 }
 
 console.log(`summarizeTv(inventory[0]):`, summarizeTv(inventory[0]));
@@ -241,8 +241,22 @@ console.log(`summarizeTv(inventory[1]):`, summarizeTv(inventory[1]));
 
 // opdracht 5b
 function formatPrice(price) {
-  return '€' + price.toFixed(0) + ',-';
+    return '€' + price.toFixed(0) + ',-';
 }
 
 console.log(`formatPrice(inventory[0].price):`, formatPrice(inventory[0].price));
 
+// opdracht 5c
+function formatSize(sizeInch) {
+    sizeCm = (2.54 * sizeInch).toFixed(0);
+    return `${sizeInch} inch (${sizeCm} cm)`;
+}
+
+function formatTvSizes(tv) {
+    return tv.availableSizes
+        .map( (size) => formatSize(size) )
+        .join(' | ');
+}
+
+console.log(`formatTvSizes(inventory[1]):`, formatTvSizes(inventory[1]));
+console.log(`formatTvSizes(inventory[5]):`, formatTvSizes(inventory[5]));
